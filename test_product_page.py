fb51289dc4a2695acb4b2f9ca5c04f1a5c572307
wswerlu <popovpn7@gmail.com>
2022-02-12 22:17:48 +0300

    refactor test_product_page.py: add parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,25 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+values = [
+            "offer0",
+            "offer1",
+            "offer2",
+            "offer3",
+            "offer4",
+            "offer5",
+            "offer6",
+            pytest.param("offer7", marks=pytest.mark.xfail),
+            "offer8",
+            "offer9",
+        ]
+
+
+@pytest.mark.parametrize("promo_value", values)
+def test_guest_can_add_product_to_basket(browser, promo_value):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={promo_value}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
