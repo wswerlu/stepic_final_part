db90bfddb1c0919474c3acca91b829deca733641
wswerlu <popovpn7@gmail.com>
2022-02-15 00:03:12 +0300

    add basket_page.py

---
+++ pages/basket_page.py
@@ -0,0 +1,13 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def should_not_be_items_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEM),\
+            "Basket is not empty but should be"
+
+    def should_be_message_about_basket_is_empty(self):
+        basket_is_empty = self.browser.find_element(*BasketPageLocators.BASKET_IS_EMPTY).get_attribute("href")
+        basket_url = self.browser.current_url
+        assert basket_is_empty in basket_url, "Basket is not empty"
