91a88a76ea1c87e0a067534dd6355f76ca5aa84c
wswerlu <popovpn7@gmail.com>
2022-02-15 00:04:19 +0300

    refactor test_main_page.py: add test test_guest_cant_see_product_in_basket_opened_from_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
-from .pages.main_page import MainPage
+from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
@@ -10,8 +11,19 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
+
+def test_guest_should_see_login_link_on_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_items_in_basket()
+    basket_page.should_be_message_about_basket_is_empty()
