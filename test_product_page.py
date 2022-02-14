97354721bf861d63c19f555e166f863346854dd4
wswerlu <popovpn7@gmail.com>
2022-02-15 00:22:39 +0300

    refactor test_product_page.py: combining tests into classes

--- test_product_page.py
+++ test_product_page.py
@@ -18,6 +18,45 @@ values = [
         ]
 
 
+class TestLoginFromProductPage:
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+class TestSuccessMessage:
+    @pytest.mark.xfail(reason="wrong test")
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.should_not_be_success_message_with_is_not_element_present()
+
+    def test_guest_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message_with_is_not_element_present()
+
+    @pytest.mark.xfail(reason="wrong test")
+    def test_message_disappeared_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.should_not_be_success_message_with_is_disappeared()
+
+
 @pytest.mark.parametrize("promo_value", values)
 def test_guest_can_add_product_to_basket(browser, promo_value):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={promo_value}"
@@ -28,47 +67,6 @@ def test_guest_can_add_product_to_basket(browser, promo_value):
     page.should_be_product_name_in_message()
     page.should_be_message_with_product_price()
     page.should_be_product_price_in_message()
-
-
-@pytest.mark.xfail(reason="wrong test")
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.should_not_be_success_message_with_is_not_element_present()
-
-
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_not_be_success_message_with_is_not_element_present()
-
-
-@pytest.mark.xfail(reason="wrong test")
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.should_not_be_success_message_with_is_disappeared()
-
-
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
