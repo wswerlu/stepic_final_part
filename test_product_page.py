de8a660da6a4bed585cf98cce5c899edf6f6a472
wswerlu <popovpn7@gmail.com>
2022-02-14 22:22:11 +0300

    refactor test_product_page.py: add tests test_guest_cant_see_success_message_after_adding_product_to_basket, test_guest_cant_see_success_message and should_not_be_success_message_with_is_disappeared

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,4 @@
 import pytest
-
 from .pages.product_page import ProductPage
 
 
@@ -11,7 +10,7 @@ values = [
             "offer4",
             "offer5",
             "offer6",
-            pytest.param("offer7", marks=pytest.mark.xfail),
+            pytest.param("offer7", marks=pytest.mark.xfail(reason="bug")),
             "offer8",
             "offer9",
         ]
@@ -27,3 +26,28 @@ def test_guest_can_add_product_to_basket(browser, promo_value):
     page.should_be_product_name_in_message()
     page.should_be_message_with_product_price()
     page.should_be_product_price_in_message()
+
+
+@pytest.mark.xfail(reason="wrong test")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message_with_is_not_element_present()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message_with_is_not_element_present()
+
+
+@pytest.mark.xfail(reason="wrong test")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message_with_is_disappeared()
