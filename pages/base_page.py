92cb1d20d887f98bc818f6147bf487f135d9fbe9
wswerlu <popovpn7@gmail.com>
2022-02-14 22:12:30 +0300

    refactor base_page.py: add methods is_not_element_present and is_disappeared

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import math
 
 
@@ -6,7 +8,7 @@ class BasePage:
     def __init__(self, browser, url, timeout=15):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+       # self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
@@ -31,3 +33,18 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(ec.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(ec.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
