f8fdf95d6355822306eef4063970f8f9f3ed6fb1
wswerlu <popovpn7@gmail.com>
2022-02-15 00:02:40 +0300

    refactor base_page.py: add method go_to_basket_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -11,6 +11,10 @@ class BasePage:
         self.url = url
         # self.browser.implicitly_wait(timeout)
 
+    def go_to_basket_page(self):
+        basket_link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        basket_link.click()
+
     def go_to_login_page(self):
         login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         login_link.click()
