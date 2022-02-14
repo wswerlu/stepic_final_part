480dcdceb0a036a0fd12e6b8e238ab366e8eaeb9
wswerlu <popovpn7@gmail.com>
2022-02-14 22:18:20 +0300

    Revert "refactor base_page.py: add tests test_guest_cant_see_success_message_after_adding_product_to_basket, test_guest_cant_see_success_message and should_not_be_success_message_with_is_disappeared"
    This reverts commit 70c756478d298901ddc31c60d949d3d4c09f89de.

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 import pytest
+
 from .pages.product_page import ProductPage
 
 
@@ -10,7 +11,7 @@ values = [
             "offer4",
             "offer5",
             "offer6",
-            pytest.param("offer7", marks=pytest.mark.xfail(reason="bug")),
+            pytest.param("offer7", marks=pytest.mark.xfail),
             "offer8",
             "offer9",
         ]
@@ -26,26 +27,3 @@ def test_guest_can_add_product_to_basket(browser, promo_value):
     page.should_be_product_name_in_message()
     page.should_be_message_with_product_price()
     page.should_be_product_price_in_message()
-
-@pytest.mark.xfail(reason="wrong test")
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.should_not_be_success_message_with_is_not_element_present()
-
-
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_not_be_success_message_with_is_not_element_present()
-
-@pytest.mark.xfail(reason="wrong test")
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.should_not_be_success_message_with_is_disappeared()
