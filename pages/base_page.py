71f6d4da0bc4f487adabbd8f9dbc7337049cb5cf
wswerlu <popovpn7@gmail.com>
2022-02-15 22:17:54 +0300

    refactor base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,10 +6,9 @@ import math
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=15):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
 
     def go_to_basket_page(self):
         basket_link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
