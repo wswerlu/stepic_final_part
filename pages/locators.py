35b70325c121148bfa9074386a2541e605508192
wswerlu <popovpn7@gmail.com>
2022-02-15 22:11:52 +0300

    refactor locators.py: add USER_ICON in class BasePageLocators, add REGISTER_BUTTON, REGISTRATION_EMAIL, REGISTRATION_PASSWORD_1, REGISTRATION_PASSWORD_2 in class LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -9,11 +9,16 @@ class BasketPageLocators:
 class BasePageLocators:
     BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn-default")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    USER_ICON = (By.CSS_SELECTOR, ".alert")
 
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "[name='registration_submit']")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
 
 
 class MainPageLocators:
