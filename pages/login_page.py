a43d9abfe0852888db23189c1474eb382c0c0ca3
wswerlu <popovpn7@gmail.com>
2022-02-15 22:12:40 +0300

    refactor login_page.py: add method register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,16 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        registration_email = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL)
+        registration_email.send_keys(email)
+        registration_password_1 = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_1)
+        registration_password_1.send_keys(password)
+        registration_password_2 = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_2)
+        registration_password_2.send_keys(password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_button.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
